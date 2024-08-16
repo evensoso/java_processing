1aa68e755ebdbbda3843665b83e6a940fcd3c08d
Sho <fuzakennamaji@gmail.com>
2024-08-16 15:25:22 +0900

    commit

--- 17_eye/ver2.java
+++ 17_eye/ver2.java
@@ -1,39 +1,40 @@
-int W=1080;
-int N=400;
+int W=1920;
+int N=960;
 float x,y,t = 0;
 
 void setup(){
 
-    size(1080,1080);
-    background(0);
-    noStroke();
-    
+    size(1920,1920);
+    background(0);
+    noStroke();
+    
 }
 
 void draw(){
-    clear();
-    background(0);
-    
-    //add
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+    clear();
+    background(0);
+    
+    //add
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
 
-            float r=TAU/N;
-            float u=sin(i+y +t)+sin(r*i+x + t);
-            float v=cos(i+y)+cos(r*i+x);
-            x=u+t;
-            y=v;
-            fill(i,j,99);
-            circle(u*N/2+W/2,y*N/2+W/2,2);
+            float r=TAU/N;
+            float u=sin(i+y +t)+sin(r*i+x + t);
+            float v=cos(i+y)+cos(r*i+x);
+            x=u+t;
+            y=v;
+            fill(i,j,99);
+            circle(u*N/2+W/2,y*N/2+W/2,2);
 
-        }
-      }
+        }
+      }
 
-      t+=0.1;
-      
-  //add----
-  String outP = "D:\\09_music_create\\03_PICTURE\\99_Render\\17_Lissajous\\2\\17_Penrose-30_######.png";
-  saveFrame(outP);
-  //----
+      t+=0.1;
+      
+  //add----
+  String rootP = "E:\\Rendered_Proc\\";
+  String outP = rootP + "17-1\\17-1_30_######.png";
+  saveFrame(outP);
+  //----
 
 }
